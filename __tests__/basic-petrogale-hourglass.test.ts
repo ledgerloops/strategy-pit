a1faea46cfaa246f8b6a14e5723b8e90678a6dcc
Michiel de Jong <michiel@pondersource.com>
2024-03-07 09:11:45 +0100

    Implement Petrogale

---
+++ __tests__/basic-petrogale-hourglass.test.ts
@@ -0,0 +1,279 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { jest } from '@jest/globals';
+
+let stage: string = "setup";
+
+describe('already friends', () => {
+  it('throws an error', async () => {
+    jest.unstable_mockModule('../src/util.js', () => {
+      return{
+        genRanHex: jest.fn((): string => {
+          return stage
+        })
+      };
+    });
+    const { Petrogale } = await import('../src/main.js');
+    stage = 'already-friends-test';
+    const alice = new Petrogale('Alice');
+    const bob = new Petrogale('Bob');
+
+    alice.meet(bob);
+    expect(() => alice.meet(bob)).toThrow('Alice is already friends with Bob');
+  });
+});
+
+describe('Basic Petrogale Hourglass', () => {
+  // let Petrogale: unknown;
+  let alice: any;
+  let bob: any;
+  let charlie: any;
+  let dave: any;
+  let edward: any;
+
+  beforeAll(async () => {
+    jest.unstable_mockModule('../src/util.js', () => {
+      return{
+        genRanHex: jest.fn((): string => {
+          return stage
+        })
+      };
+    });
+    const { Petrogale } = await import('../src/main.js');
+    stage = "hourglass-setup"
+    alice = new Petrogale('Alice');
+    bob = new Petrogale('Bob');
+    charlie = new Petrogale('Charlie');
+    dave = new Petrogale('Dave');
+    edward = new Petrogale('Edward');
+  });
+
+  describe('Triangle, then Alice meets Dave', () => {
+    beforeAll(() => {
+      // First go through the steps of the Basic Petrogale Triangle test:
+      stage = "AliceBob";
+      alice.meet(bob);
+      stage = "BobCharlie";
+      bob.meet(charlie);
+      stage = "CharlieAlice";
+      charlie.meet(alice);
+      // Now add Dave:
+      stage = "AliceDave";
+      alice.meet(dave);
+    });
+    it('Alice is friends with triangle + Dave', () => {
+      expect(alice.getFriends().sort()).toEqual([
+        'Bob',
+        'Charlie',
+        'Dave'
+      ]);
+    });
+    it('Alice has the triangle probes plus an AliceDave probe for Dave', () => {
+      expect(alice.getProbes()).toEqual({
+        AliceBob: { Bob: true, Charlie: true, Dave: true },
+        BobCharlie: { Bob: true, Charlie: true, Dave: true },
+        CharlieAlice: { Bob: true, Charlie: true, Dave: true },
+        AliceDave: { Bob: true, Charlie: true, Dave: true }
+    });
+    });
+    it('Alice has the triangle loops', () => {
+      expect(alice.getLoops().sort()).toEqual([
+        'AliceBob:CharlieAlice',
+        'BobCharlie:CharlieAlice',
+        'CharlieAlice:CharlieAlice',
+        'AliceDave:AliceDave'
+      ].sort());
+    });
+
+    describe('Dave meets Edward', () => {
+      beforeAll(() => {
+        stage = "DaveEdward";
+        dave.meet(edward);
+      });
+      it('Alice is friends with the triangle plus Dave', () => {
+        expect(alice.getFriends().sort()).toEqual([ 'Bob', 'Charlie', 'Dave' ].sort());
+      });
+      it('Alice has probes for the triangle plus Dave and Edward', () => {
+        expect(alice.getProbes()).toEqual({
+          AliceBob: { Bob: true, Charlie: true, Dave: true },
+          BobCharlie: { Bob: true, Charlie: true, Dave: true },
+          CharlieAlice: { Bob: true, Charlie: true, Dave: true },
+          AliceDave: { Bob: true, Charlie: true, Dave: true },
+          DaveEdward: { Bob: true, Charlie: true, Dave: true }
+        });
+      });
+      it('Dave is friends with Alice and Edward', () => {
+        expect(dave.getFriends().sort()).toEqual([ 'Alice', 'Edward' ].sort());
+      });
+      it('Dave has all the triangle probes plus one for Edward', () => {
+        expect(dave.getProbes()).toEqual({
+          AliceBob: { Alice: true, Edward: true },
+          BobCharlie: { Alice: true, Edward: true },
+          CharlieAlice: { Alice: true, Edward: true },
+          AliceDave: { Alice: true, Edward: true },
+          DaveEdward: { Alice: true, Edward: true }
+        });
+      });
+      it('Edward is friends with Dave', () => {
+        expect(edward.getFriends().sort()).toEqual([ 'Dave' ]);
+      });
+      it('Edward has all the triangle probes plus one for Dave', () => {
+        expect(edward.getProbes()).toEqual({
+          AliceBob: { Dave: true },
+          AliceDave: { Dave: true },
+          BobCharlie: { Dave: true },
+          CharlieAlice: { Dave: true },
+          DaveEdward: { Dave: true }
+        });
+      });
+   
+      describe('Edward meets Alice', () => {
+        beforeAll(() => {
+          stage = "EdwardAlice";
+          edward.meet(alice);
+        });
+
+        it('Alice is friends with everyone', () => {
+          expect(alice.getFriends().sort()).toEqual([ 'Bob', 'Charlie', 'Dave', 'Edward' ]);
+        });
+
+        it('Alice has probes for each hourglass edge', () => {
+          expect(alice.getProbes()).toEqual({
+            AliceBob: { Bob: true, Charlie: true, Dave: true, Edward: true },
+            BobCharlie: { Bob: true, Charlie: true, Dave: true, Edward: true },
+            CharlieAlice: { Bob: true, Charlie: true, Dave: true, Edward: true },
+            AliceDave: { Bob: true, Charlie: true, Dave: true, Edward: true },
+            DaveEdward: { Bob: true, Charlie: true, Dave: true, Edward: true },
+            EdwardAlice: { Bob: true, Charlie: true, Dave: true, Edward: true }
+          });
+        });
+        it('Alice has 11 loops', () => {
+          expect(alice.getLoops().sort()).toEqual([
+            'AliceBob:CharlieAlice',
+            'AliceBob:EdwardAlice',
+            'BobCharlie:CharlieAlice',
+            'BobCharlie:EdwardAlice',
+            'CharlieAlice:CharlieAlice',
+            'CharlieAlice:EdwardAlice',
+            'AliceDave:AliceDave',
+            'AliceDave:EdwardAlice',
+            'DaveEdward:DaveEdward',
+            'DaveEdward:EdwardAlice',
+            'EdwardAlice:EdwardAlice'
+          ].sort());
+        });
+   
+        it('Bob is friends with Alice and Charlie', () => {
+          expect(bob.getFriends().sort()).toEqual([ 'Alice', 'Charlie' ]);
+        });
+   
+        it('Bob has one probe for each hourglass edge', () => {
+          expect(bob.getProbes()).toEqual({
+            AliceBob: { Alice: true, Charlie: true },
+            BobCharlie: { Alice: true, Charlie: true },
+            CharlieAlice: { Alice: true, Charlie: true },
+            AliceDave: { Alice: true, Charlie: true },
+            DaveEdward: { Alice: true, Charlie: true },
+            EdwardAlice: { Alice: true, Charlie: true }
+          }); 
+        });
+        it('Bob has 11 loops', () => {
+          expect(bob.getLoops().sort()).toEqual([
+            'AliceBob:CharlieAlice',
+            'AliceBob:EdwardAlice',
+            'BobCharlie:CharlieAlice',
+            'BobCharlie:EdwardAlice',
+            'CharlieAlice:CharlieAlice',
+            'CharlieAlice:EdwardAlice',
+            'AliceDave:AliceDave',
+            'AliceDave:EdwardAlice',
+            'DaveEdward:DaveEdward',
+            'DaveEdward:EdwardAlice',
+            'EdwardAlice:EdwardAlice'
+          ].sort());
+        });
+
+        it('Charlie is friends with Bob and Alice', () => {
+          expect(charlie.getFriends().sort()).toEqual([ 'Bob', 'Alice' ].sort());
+        });
+
+        it('Charlie has one probe for each hourglass edge', () => {
+          expect(charlie.getProbes()).toEqual({
+            AliceBob: { Alice: true, Bob: true },
+            BobCharlie: { Alice: true, Bob: true },
+            CharlieAlice: { Alice: true, Bob: true },
+            AliceDave: { Alice: true, Bob: true },
+            DaveEdward: { Alice: true, Bob: true },
+            EdwardAlice: { Alice: true, Bob: true }
+          });
+        });
+        it('Charlie has 11 loops', () => {
+          expect(charlie.getLoops().sort()).toEqual([
+            'AliceBob:CharlieAlice',
+            'AliceBob:EdwardAlice',
+            'AliceDave:AliceDave',
+            'AliceDave:EdwardAlice',
+            'BobCharlie:CharlieAlice',
+            'BobCharlie:EdwardAlice',
+            'CharlieAlice:CharlieAlice',
+            'CharlieAlice:EdwardAlice',
+            'DaveEdward:DaveEdward',
+            'DaveEdward:EdwardAlice',
+            'EdwardAlice:EdwardAlice'
+          ].sort());
+        });
+
+        it('Dave is friends with Edward and Alice', () => {
+          expect(dave.getFriends().sort()).toEqual([ 'Edward', 'Alice' ].sort());
+        });
+
+        it('Dave has probes for the second triangel of the hourglass edge', () => {
+          expect(dave.getProbes()).toEqual({
+            AliceBob: { Alice: true, Edward: true },
+            BobCharlie: { Alice: true, Edward: true },
+            CharlieAlice: { Alice: true, Edward: true },
+            AliceDave: { Alice: true, Edward: true },
+            DaveEdward: { Alice: true, Edward: true },
+            EdwardAlice: { Alice: true, Edward: true }
+          });
+        });
+        it('Dave has 6 loops', () => {
+          expect(dave.getLoops().sort()).toEqual([
+            'AliceBob:EdwardAlice',
+            'BobCharlie:EdwardAlice',
+            'CharlieAlice:EdwardAlice',
+            'AliceDave:EdwardAlice',
+            'DaveEdward:EdwardAlice',
+            'EdwardAlice:EdwardAlice'
+          ].sort());
+        });
+
+        it('Edward is friends with Dave and Alice', () => {
+          expect(edward.getFriends().sort()).toEqual([ 'Dave', 'Alice' ].sort());
+        });
+
+        it('Edward has probes for the second triangle of the hourglass edge', () => {
+          expect(edward.getProbes()).toEqual({
+            AliceBob: { Alice: true, Dave: true },
+            BobCharlie: { Alice: true, Dave: true },
+            CharlieAlice: { Alice: true, Dave: true },
+            AliceDave: { Alice: true, Dave: true },
+            DaveEdward: { Alice: true, Dave: true },
+            EdwardAlice: { Alice: true, Dave: true }
+          });
+        });
+        it('Edward has 6 loops', () => {
+          expect(edward.getLoops().sort()).toEqual([
+            'AliceBob:EdwardAlice',
+            'AliceDave:EdwardAlice',
+            'BobCharlie:EdwardAlice',
+            'CharlieAlice:EdwardAlice',
+            'DaveEdward:EdwardAlice',
+            'EdwardAlice:EdwardAlice'
+          ].sort());
+        });
+
+      }); // Edward meets Alice
+    }); // Dave meets Edward
+  }); // Alice meets Dave
+}); // basic petrogale hourglass
---
+++ src/petrogale.ts
@@ -0,0 +1,35 @@
+import { Meet, Probe } from "./messages.js";
+import { genRanHex } from "./util.js";
+import { Node } from "./node.js";
+import { Pelican } from "./pelican.js";
+
+export class Petrogale extends Pelican {
+  constructor(name: string) {
+    super(name);
+  }
+  meet(other: Node): void {
+    this.addFriend(other);
+    other.receiveMessage(new Meet(this as Node));
+
+    // create new probe for new link
+    const probeForNewLink = genRanHex(8);
+    if (typeof this.probes[probeForNewLink] === 'undefined') {
+      this.probes[probeForNewLink] = {};
+    }
+    Object.values(this.friends).forEach(friend => {
+      if (typeof this.probes[probeForNewLink][friend.getName()] === 'undefined') {
+        this.probes[probeForNewLink][friend.getName()] = true;
+        friend.receiveMessage(new Probe(this as Node, probeForNewLink));
+        return;
+      }
+    });
+
+    // send existing probes to new friend
+    Object.entries(this.probes).forEach(([id, probes]) => {
+      if (typeof probes[other.getName()] === 'undefined') {
+        this.probes[id][other.getName()] = true;
+        other.receiveMessage(new Probe(this, id));
+      }
+    });
+  }
+}
