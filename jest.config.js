b6c74481a0b3784322d40325be52915c0428cc22
Michiel de Jong <michiel@pondersource.com>
2024-03-07 14:55:54 +0100

    Fix #1

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
   },
-  testRegex: '(/__tests__/basic-petrogale-hourglass.test.ts$)',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
