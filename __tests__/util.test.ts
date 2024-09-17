f9be3da081bf1cd6328842fc7ce7262d6ee67ce1
Michiel de Jong <michiel@pondersource.com>
2024-09-17 14:05:34 +0200

    random but deterministic words as genRanHex values

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -2,6 +2,7 @@ import { genRanHex } from "../src/util.js";
 
 describe('genRanHex', () => {
     it('returns a string of the correct length', () => {
-        expect(genRanHex(4).length).toBe(4);
+        // expect(genRanHex(4).length).toBe(4);
+        expect(genRanHex(4)).toBe('confess');
     });
 });
--- src/genRanHex.ts
+++ src/genRanHex.ts
@@ -1,3 +1,11 @@
-export function genRanHex(size: number): string {
-  return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+import { getWords } from './randomWords.js';
+
+let counter: number = 0;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function genRanHex(_size: number): string {
+  const words = getWords();
+  // return words[Math.floor(Math.random() * words.length)];
+  return words[counter++];
+  // return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 }
