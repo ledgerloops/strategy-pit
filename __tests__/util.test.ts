9a83fbcde86c5cba76fb38da5fcaf5a449251c82
Michiel de Jong <michiel@pondersource.com>
2024-10-18 11:40:17 +0200

    Remove older stuff

--- __tests__/util.test.ts
+++
@@ -1,7 +0,0 @@
-import { genRanHex } from "../src/util.js";
-
-describe('genRanHex', () => {
-    it('returns a string of the correct length', () => {
-        expect(genRanHex(4).length).toBe(4);
-    });
-});
--- src/genRanHex.ts
+++
@@ -1,3 +0,0 @@
-export function genRanHex(size: number): string {
-  return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-}
